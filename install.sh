3fcf0a41e409bf668cfab4b25585f6e1f5320f98
Alvaro Lizama <nekrox@gmail.com>
2018-08-25 13:55:17 -0500

    Update readme and install

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ ln -s $HOME/.dotfiles/dotfiles/vim $HOME/.vim
 ln -s $HOME/.dotfiles/dotfiles/vim/init.vim $HOME/.vimrc
 ln -s $HOME/.dotfiles/dotfiles/zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/dotfiles/tmux.conf $HOME/.tmux.conf
-
+ln -s $HOME/.dotfiles/dotfiles/gitconfig $HOME/.gitconfig
 
 
